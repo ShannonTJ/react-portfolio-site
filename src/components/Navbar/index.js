182376c52cb8e17cc2e7bb8d71d7fc733164fe41
Shannon TJ <shannontj@shaw.ca>
2020-12-27 10:58:07 -0700

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Nav } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      {/* <NavbarContainer>
+        <NavTextWrapper>
+          <LeftText></LeftText>
+          <RightText></RightText>
+        </NavTextWrapper>
+      </NavbarContainer> */}
+    </Nav>
+  );
+};
+
+export default Navbar;
