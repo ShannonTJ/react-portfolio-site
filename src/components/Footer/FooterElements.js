4e624748de781b131b400f37131e047560569ec3
Shannon TJ <shannontj@shaw.ca>
2020-12-28 17:02:37 -0700

    adding footer styling

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -3,13 +3,36 @@ import styled from "styled-components";
 export const FooterContainer = styled.footer`
   background: #000;
   bottom: 0;
-  height: 100px;
+  height: 80px;
   width: 100%;
   position: absolute;
+  display: flex;
 `;
 
-export const FooterTextWrapper = styled.div``;
+export const FooterTextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+`;
 
-export const FooterText = styled.p`
+export const FooterText = styled.small`
   color: #fff;
+  font-size: 1.2rem;
+  font-weight: 100;
+`;
+
+export const FooterIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 0 0 0;
+  width: 50%;
+`;
+
+export const FooterIcon = styled.a`
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 100;
+  display: inline-block;
 `;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,14 +3,35 @@ import {
   FooterContainer,
   FooterTextWrapper,
   FooterText,
+  FooterIconWrapper,
+  FooterIcon,
 } from "./FooterElements";
+import { FaGithubAlt, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <FooterContainer>
       <FooterTextWrapper>
-        <FooterText>Shannon copyright</FooterText>
+        <FooterText>
+          shannon © {new Date().getFullYear()} all rights reserved
+        </FooterText>
       </FooterTextWrapper>
+      <FooterIconWrapper>
+        <FooterIcon
+          href="https://github.com/ShannonTJ"
+          target="_blank"
+          aria-label="Github"
+        >
+          <FaGithubAlt />
+        </FooterIcon>
+        <FooterIcon
+          href="mailto:shannontjgraphics@gmail.com"
+          target="_blank"
+          aria-label="Mail"
+        >
+          <FaEnvelope />
+        </FooterIcon>
+      </FooterIconWrapper>
     </FooterContainer>
   );
 };
