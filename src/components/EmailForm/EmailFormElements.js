8225b5a0af59a387a5d3703675cc9ea148b06370
Shannon TJ <shannontj@shaw.ca>
2021-01-15 16:07:42 -0700

    email form styling

--- src/components/EmailForm/EmailFormElements.js
+++ src/components/EmailForm/EmailFormElements.js
@@ -1,13 +1,89 @@
 import styled from "styled-components/macro";
 
-export const FormContainer = styled.div``;
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8rem;
+  margin-bottom: 2rem;
+  max-width: 100%;
 
-export const FormContentContainer = styled.div``;
+  @media screen and (max-width: 480px) {
+    margin-bottom: 4rem;
+  }
+`;
 
-export const InputContainer = styled.div``;
+export const FormContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #5d6d7e;
+  padding: 30px;
+  border-radius: 10px;
+  width: 400px;
+  height: 100%;
+`;
 
-export const NameInput = styled.div``;
-export const EmailInput = styled.div``;
-export const MessageInput = styled.div``;
+export const InputContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-export const SubmitButton = styled.button``;
+export const NameInput = styled.input`
+  margin-top: 32px;
+  padding: 16px 140px 16px 10px;
+  margin-bottom: 32px;
+  border: none;
+  border-radius: 4px;
+
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+`;
+export const EmailInput = styled.input`
+  padding: 16px 140px 16px 10px;
+  margin-bottom: 32px;
+  border: none;
+  border-radius: 4px;
+
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+`;
+export const MessageInput = styled.textarea`
+  padding: 16px 120px 16px 10px;
+  margin-bottom: 32px;
+  border: none;
+  border-radius: 4px;
+
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+`;
+
+export const SubmitButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 600;
+  background-color: #5484aa;
+  color: #fff;
+
+  padding: 16px 60px;
+  margin-bottom: 32px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease-in;
+
+  &:hover {
+    background-color: #fff;
+    color: #5484aa;
+  }
+`;
--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   FormContainer,
   FormContentContainer,
@@ -14,13 +14,22 @@ const EmailForm = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  const sendMessage = (event) => {
+    event.preventDefault();
+    console.log(name, email, message);
+
+    event.target.reset();
+  };
+
   return (
     <FormContainer>
       <FormContentContainer>
-        <InputContainer>
+        <InputContainer onSubmit={sendMessage}>
           <NameInput
             id="name"
             name="name"
+            placeholder="Your name"
+            required
             onChange={(event) => {
               event.preventDefault();
               setName(event.target.value);
@@ -29,6 +38,8 @@ const EmailForm = () => {
           <EmailInput
             id="email"
             name="email"
+            placeholder="Your email"
+            required
             onChange={(event) => {
               event.preventDefault();
               setEmail(event.target.value);
@@ -37,13 +48,19 @@ const EmailForm = () => {
           <MessageInput
             id="message"
             name="message"
+            placeholder="Your message"
+            rows="10"
+            columns="50"
+            required
             onChange={(event) => {
               event.preventDefault();
               setMessage(event.target.value);
             }}
           />
         </InputContainer>
-        <SubmitButton />
+        <SubmitButton value="send" type="submit">
+          Send
+        </SubmitButton>
       </FormContentContainer>
     </FormContainer>
   );
